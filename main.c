ef4fcb0ec758f330d6ab1435546e54b636ea8a2b
Anurag Verma <anu.anuragverma@gmail.com>
2020-10-17 14:21:53 +0530

    Fix exit on Deletion

--- main.c
+++ main.c
@@ -1,11 +1,3 @@
-/**
- * 
- * Project structure is going to look something like this.
- * Make sure you have your code defragmented into smaller bits for easier integration and testing. 
- * 
- * 
- **/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "auth/auth.h"
@@ -66,5 +58,5 @@ int main(){
             default: printf ("\nWrong choice. Please try again !\n");                   
         }
     }
-    while (ch != 3);
+    while (ch != 5);
 }
